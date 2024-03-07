c10594be932b4a41cd7d2ce8141366c92b7e18a2
huali0809 <1132457928@qq.com>
2024-03-07 15:58:48 +0800

    用户上线功能: 广播上线端口上线信息

--- 02instantMsg/main.go
+++ 02instantMsg/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+
+	//gin第1种方法
+	//serverGin := gin.Default()
+	//serverGin.GET("/", func(context *gin.Context) {
+	//	context.String(http.StatusOK, "hello gin server")
+	//})
+	//serverGin.Run("8081")
+
+	//gin第2种方法
+	//serverGin := gin.Default()
+	//uHandler := &UserHandler{}
+	//uHandler.RegisterRoutes(serverGin)
+	//serverGin.Run("8081")
+
 	server := NewServer("127.0.0.1", 8080)
 	server.Start()
 	fmt.Println("hello world go")
--- 02instantMsg/service.go
+++ 02instantMsg/service.go
@@ -3,26 +3,62 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	//在线用户的列表
+	OnLineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	Message chan string
 }
 
 // NewServer 新建一个 提供创建一个server的接口
 func NewServer(ip string, prot int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: prot,
+		Ip:        ip,
+		Port:      prot,
+		OnLineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 
 	return server
 }
 
+func (s *Server) ListenMessage() {
+	for {
+		msg := <-s.Message
+
+		s.mapLock.Lock()
+		for _, cli := range s.OnLineMap {
+			cli.C <- msg
+		}
+		s.mapLock.Unlock()
+	}
+}
+
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	s.Message <- sendMsg
+}
+
 func (s *Server) Handler(conn net.Conn) {
 	//在这里写 命中的业务
-	fmt.Println("链接建立成功")
+	//fmt.Println("链接建立成功")
+
+	user := NewUser(conn)
+
+	//用户上线, 将用户加入到oneLineMap中
+	s.mapLock.Lock()
+	s.OnLineMap[user.Name] = user
+	s.mapLock.Unlock()
+
+	s.BroadCast(user, "is online")
 }
 
 func (s *Server) Start() {
@@ -31,6 +67,8 @@ func (s *Server) Start() {
 		fmt.Println("net.Listen err:", err)
 	}
 	defer l.Close()
+
+	go s.ListenMessage()
 
 	for {
 		conn, err := l.Accept()
