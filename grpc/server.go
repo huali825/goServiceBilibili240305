f536415f74f783a3790869ce394bfe844064fa99
huali0809 <1132457928@qq.com>
2024-08-23 20:32:14 +0800

    grpc的 server结构体

--- grpc/server.go
+++ grpc/server.go
@@ -1,7 +1,21 @@
 package grpc
 
-import "go20240218/grpc/service"
+import (
+	"context"
+	"go20240218/grpc/service"
+)
 
 type Server struct {
 	service.UnimplementedUserServiceServer
+}
+
+var _ service.UserServiceServer = &Server{} // 检查Server是否实现了UserserviceServer接口
+
+func (s *Server) GetById(ctx context.Context, request *service.GetByIdRequest) (*service.GetByIdResponse, error) {
+	return &service.GetByIdResponse{
+		User: &service.User{
+			Age:      123,
+			Username: "abcd",
+		},
+	}, nil
 }
